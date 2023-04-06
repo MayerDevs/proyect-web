561400f2d51109d15fd5a043cee2f54a99dae071
mayer malpud <129673098+1201104027@users.noreply.github.com>
2023-04-05 22:39:02 -0500

    objetos en js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 function hola(){
     console.log("hola, mundo")
 }
-
 // hola()
 
 const saludo = (x) => {
@@ -14,5 +13,33 @@ const saludo = (x) => {
 const suma = (x=0,y=3) => {
     return `la suma es ${x+y}`
 }
+// console.log(suma())
 
-console.log(suma())
+
+// objectos en js
+const user = {
+    name: "jair",
+    cel: 232132,
+    address: {
+        cra: 23,
+        no: '34-53',
+        barrio: "lorenzo"
+    },
+    works: {
+        fisrt: 'IS',
+        second: 'doctor'
+    },
+    send(){
+        return "sending.."
+    }
+}
+// console.log(user.send())
+
+const name = "rice"
+const price = 2000
+
+const Product = {
+    name,
+    price
+}
+// console.log(`el producto es ${name} y el costo es ${price}`)
