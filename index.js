502c304179d9676c22545fe4bffc87858209f0b3
mayer malpud <129673098+1201104027@users.noreply.github.com>
2023-04-05 23:21:37 -0500

    manejo de DOM y destructura de datos

--- index.js
+++ index.js
@@ -43,3 +43,37 @@ const Product = {
     price
 }
 // console.log(`el producto es ${name} y el costo es ${price}`)
+
+
+// DOM
+const input = document.createElement('input')
+const button = document.createElement('button')
+const div = document.createElement('div')
+
+button.innerText = "button"
+button.style = "background-color: red; border: 0; margin-left: 15px; cursor: pointer"
+
+document.body.append(input)
+document.body.append(button)
+document.body.append(div)
+
+// escuchas event-handlers
+button.addEventListener("click", () => {
+    const value = document.querySelector('input').value
+    console.log(value)
+    alert(`boton escuchando el valor del input es: ${value}`)
+})
+
+//destructura de datos
+const student = {
+    name: "carlos",
+    age: 23
+} 
+
+function info ({age}){
+    console.log(`<h1>nombre: ${student['name']}, edad ${student.age} /h1>`)
+    return '<h1>'+'edad: '+age+'</h1>'
+}
+
+// div.innerHTML = info(student)
+// console.log(info())
